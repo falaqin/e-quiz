4b861d81ef8054487f96587300c3e1802ee06535
obake199 <roleanite@gmail.com>
2021-11-14 10:29:04 +0800

    init

--- supervisor/quiz_manage.php
+++ supervisor/quiz_manage.php
@@ -27,47 +27,58 @@ $res=mysqli_fetch_assoc($query);
 
     <!-- Modal -->
     <div class="modal fade text-dark" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Add Question</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <form action="" method="post">
-                <div class="form-group">
-                    <label for="question">Question</label>
-                    <input type="text" name="question" class="form-control">
-                </div>
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Question</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" method="post">
+                    <div class="form-group">
+                        <label for="question">Question</label>
+                        <input type="text" name="question" class="form-control">
+                    </div>
 
-                <div class="form-group">
-                    <label for="answer1">Answer</label>
-                    <input type="text" name="answer1" class="form-control">
-                    <input type="radio" name="correct1" id="">
-                </div>
+                    <div class="form-group">
+                        <label for="answer1">Answer</label>
+                        <input type="text" name="answer1" class="form-control">
+                        <input type="radio" name="correct1" id="">
+                        <label for="correct1">Correct answer</label>
+                    </div>
 
-                <div class="form-group">
-                    <label for="answer2">Answer</label>
-                    <input type="text" name="answer2" class="form-control">
-                </div>
+                    <div class="form-group">
+                        <label for="answer2">Answer</label>
+                        <input type="text" name="answer2" class="form-control">
+                        <input type="radio" name="correct2" id="">
+                        <label for="correct2">Correct answer</label>
+                    </div>
 
-                <div class="form-group">
-                    <label for="answer3">Answer</label>
-                    <input type="text" name="answer3" class="form-control">
-                </div>
+                    <div class="form-group">
+                        <label for="answer3">Answer</label>
+                        <input type="text" name="answer3" class="form-control">
+                        <input type="radio" name="correct3" id="">
+                        <label for="correct3">Correct answer</label>
+                    </div>
 
-                <div class="form-group">
-                    <label for="answer4">Answer</label>
-                    <input type="text" name="answer4" class="form-control">
-                </div>
-            </form>
+                    <div class="form-group">
+                        <label for="answer4">Answer</label>
+                        <input type="text" name="answer4" class="form-control">
+                        <input type="radio" name="correct4" id="">
+                        <label for="correct4">Correct answer</label>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
         </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+
+        <div>
+            
         </div>
-        </div>
-    </div>
     </div>
 
     <style>
