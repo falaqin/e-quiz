ececae1375aa1ee5ec9c41564aad9d17732ec74f
obake199 <roleanite@gmail.com>
2021-12-01 14:29:18 +0800

    update

--- lecturer/quiz_student_delete.php
+++ lecturer/quiz_student_delete.php
@@ -3,8 +3,8 @@
 include('../inc/database.php');
 
 //Get id
-$std_id = $_GET['std_id'];
-$quiz_id = $_GET['quiz_id'];
+    $std_id = $_GET['std_id'];
+    $quiz_id = $_GET['quiz_id'];
 
 //SQL statement
 $sql="DELETE FROM student_quiz WHERE quiz_id = '$quiz_id' AND std_id='$std_id'";
